53b60470d41a41467e6428dedcb2efa209c04c1b
unseon5470 <unseon5470@naver.com>
2021-05-16 00:47:23 +0900

    Ready Draw Graph using DataTable Live Data

--- ChartSampleApp/MainWindow.xaml.cs
+++ ChartSampleApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,10 +21,48 @@ namespace ChartSampleApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread thDataRowAdd = null;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            ui_usLineChart.EnterLock();
+            //Initialize Chart Datas
             ui_usLineChart.ChartTitle = "Line Chart Sample";
+            //Set Chart Column Information
+            ui_usLineChart.dataTable.Columns.Add("DateTime",typeof(DateTime));
+            ui_usLineChart.dataTable.Columns.Add("Level",typeof(Double));
+            ui_usLineChart.ExitLock();
+
+            if (thDataRowAdd == null)
+                thDataRowAdd = new Thread(new ThreadStart(AddChartDataAuto));
+
         }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            //Close Process All.
+            Application.Current.Shutdown();
+        }
+
+        private double startLevel = 100;
+        private void AddChartDataAuto()
+        {
+            Random random = new Random();
+            while(true)
+            {
+                startLevel += 1-(random.NextDouble()*2);
+                ui_usLineChart.EnterLock();
+                ui_usLineChart.dataTable.Rows.Add(DateTime.Now, startLevel);
+                if(ui_usLineChart.dataTable.Rows.Count>1000)
+                {
+                    ui_usLineChart.dataTable.Rows.RemoveAt(0);
+                }
+                ui_usLineChart.ExitLock();
+                Thread.Sleep(50);
+            }
+        }
+
     }
 }
--- UnseonChartLib/USCL/USLineChart.xaml.cs
+++ UnseonChartLib/USCL/USLineChart.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,7 +27,20 @@ namespace UnseonChartLib.USCL
         {
             InitializeComponent();
             ChartOuterLineBrush = new SolidColorBrush(Color.FromArgb(255, 50, 50, 50));
+            dataTable = new DataTable();
         }
+
+        private Object _locker = new Object();
+        public void EnterLock()
+        {
+            Monitor.Enter(_locker);
+        }
+
+        public void ExitLock()
+        {
+            Monitor.Exit(_locker);
+        }
+
 
         private void ui_canvas_Loaded(object sender, RoutedEventArgs e)
         {
@@ -34,6 +49,8 @@ namespace UnseonChartLib.USCL
 
         private void UpdateUI(object sender, EventArgs e)
         {
+            EnterLock();
+            
             //Assemble Objects base on Canvas
             Assembly();
 
@@ -42,15 +59,20 @@ namespace UnseonChartLib.USCL
 
             //Update Objects Positions
             PositionUpdate();
+           
+            ExitLock();
         }
 
-       
+        
+
         private StackPanel ui_section_header = new StackPanel();
         private Canvas ui_section_body = new Canvas();
         private TextBlock ui_title = new TextBlock();
         private Line ui_line_bottom = new Line();
         private Line ui_line_right = new Line();
         private List<Polyline> ui_polylines = new List<Polyline>();
+        private List<TextBlock> ui_xLabels = new List<TextBlock>();
+        private List<TextBlock> ui_yLabels = new List<TextBlock>();
 
         public void Assembly()
         {
@@ -66,13 +88,47 @@ namespace UnseonChartLib.USCL
             //add ui_section_chartbody
             USCommon.AssemblySingle(ui_canvas, ui_section_body);
             
-            
-            
             //add ui_title on ui_section_header
             USCommon.AssemblySingle(ui_section_header, ui_title);
 
-           
+            //add chart graph
+            if(dataTable!=null && dataTable.Columns.Count>=2 && dataTable.Rows.Count > 0)
+            {
+                //chart zoom setup
+                int startRowNum = 0;
+                int endRowNum = dataTable.Rows.Count - 1;
+
+                //calculate chart label position period
+                double xLabelWidthPeriod = 80;
+                double yLabelHeightPeriod = 80;
+                double xLabelCountPeriod = ui_section_body.ActualWidth / xLabelWidthPeriod;
+                double yLabelCountPeriod = ui_section_body.ActualHeight / yLabelHeightPeriod;
+
+                //chart data columns fetch
+                for (int i = 1; i < dataTable.Columns.Count; i++)
+                {
+                    if (dataTable.Columns[0].DataType.Equals(typeof(DateTime)))
+                    {
+
+                    }
+                    else if (dataTable.Columns[0].DataType.Equals(typeof(int)))
+                    {
+
+                    }
+                    else if (dataTable.Columns[0].DataType.Equals(typeof(String)))
+                    {
+
+                    }
+                }
+
+            }
+
+            
+
         }
+
+        //Please Use Format : EnterLock(); access _dataTable ExitLock(); 
+        public DataTable dataTable { get; set; }
 
         public string ChartTitle { get; set; }
         public Brush ChartOuterLineBrush { get; set; }
