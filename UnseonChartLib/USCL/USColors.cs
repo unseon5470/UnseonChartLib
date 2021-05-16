c7b95c521e54db73c6d72742b4acff9d931ccc79
unseon5470 <unseon5470@google.com>
2021-05-16 21:30:39 +0900

    Optimize

--- UnseonChartLib/USCL/USColors.cs
+++ UnseonChartLib/USCL/USColors.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnseonChartLib.USCL
 {
@@ -14,7 +10,6 @@ namespace UnseonChartLib.USCL
 
         public static Color GetColor(int i)
         {
-
             if (colorCodes.Length > i)
                 return Color.FromName(colorCodes[i]);
             else
